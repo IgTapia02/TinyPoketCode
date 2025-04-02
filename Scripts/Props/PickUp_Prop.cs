f52d4b50c26709ba7701f6beca02f03478ae4d7b
Ignacio Tapia <114392929+IgTapia02@users.noreply.github.com>
2025-04-02 18:31:14 +0200

    Update PickUp_Prop.cs

--- Scripts/Props/PickUp_Prop.cs
+++ Scripts/Props/PickUp_Prop.cs
@@ -4,31 +4,6 @@ using UnityEngine;
 
 public class PickUp_Prop : MonoBehaviour
 {
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-    //        var playerActions = collision.gameObject.GetComponent<Player_Actions>();
-    //        playerActions.overObject = true;
-    //        playerActions.Item = this.gameObject;
-    //    }
-    //}
-
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-    //        var playerActions = collision.gameObject.GetComponent<Player_Actions>();
-    //        // Solo actualiza si el item actual es este objeto
-    //        if (playerActions.Item == this.gameObject)
-    //        {
-    //            playerActions.overObject = false;
-    //            playerActions.Item = null;
-    //        }
-    //    }
-    //}
-
     void Start()
     {
     }
